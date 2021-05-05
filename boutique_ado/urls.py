ef930bb08aff864f5682b8d3d6dd7aa3bdad2549
robot5000-design <ducativtwin@gmail.com>
2021-05-05 22:13:29 +0000

    Add homepage content

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # from allauth docs
+    path('', include('home.urls')),
 ]
