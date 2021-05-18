7e4af28ecd2c59eb970cb70966a80a543f17150c
robot5000-design <ducativtwin@gmail.com>
2021-05-18 23:09:59 +0000

    Profiles part 4 - added order history and updated checkout views

--- checkout/admin.py
+++ checkout/admin.py
@@ -16,7 +16,7 @@ class OrderAdmin(admin.ModelAdmin):
                        'stripe_pid')
 
     # optional to use fields but it keeps to order of how they appear in the admin page exactly how we want them
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
